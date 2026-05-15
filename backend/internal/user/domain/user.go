22a117b69b49360490dd00a6d93835b53449c84c
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-05-15 17:07:34 +0700

    Implement UserRepository contract that Domain layer defined

--- backend/internal/user/domain/user.go
+++ backend/internal/user/domain/user.go
@@ -21,7 +21,7 @@ var (
 )
 
 // Infrastructure interface
-type UserInfrastructure interface {
+type UserRepository interface {
 	Create(ctx context.Context, user *User) error
 	GetByEmail(ctx context.Context, email string) (*User, error)
 	GetByID(ctx context.Context, id int64) (*User, error)
