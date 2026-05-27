4180d0b32752cd57005a9967c0d9720a5cf33fd2
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-05-27 15:02:41 +0700

    Add IF NOT EXISTS to DB to prevent initialization errors

--- backend/database/migration/schema.sql
+++ backend/database/migration/schema.sql
@@ -1,5 +1,5 @@
 -- Table 1 (User info)
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id BIGSERIAL PRIMARY KEY,
 	full_name VARCHAR(255) NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE users (
 );
 
 -- Table 2 (Store balance)
-CREATE TABLE wallets (
+CREATE TABLE IF NOT EXISTS wallets (
 	id BIGSERIAL PRIMARY KEY,
 	user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 	balance BIGINT NOT NULL DEFAULT 0,
@@ -18,7 +18,7 @@ CREATE TABLE wallets (
 );
 
 -- Table 3 (Store transfer info between 2 wallets)
-CREATE TABLE transfers (
+CREATE TABLE IF NOT EXISTS transfers (
 	id BIGSERIAL PRIMARY KEY,
 	from_wallet_id BIGINT NOT NULL REFERENCES wallets(id),
     to_wallet_id BIGINT NOT NULL REFERENCES wallets(id),
@@ -28,14 +28,14 @@ CREATE TABLE transfers (
 
 -- Table 4 (Record all positive/negative changes of each wallet)
 -- Used to audit. The sum of all entries of 1 wallet must equal the balance of this wallet
-CREATE TABLE entries (
+CREATE TABLE IF NOT EXISTS entries (
     id BIGSERIAL PRIMARY KEY,
     wallet_id BIGINT NOT NULL REFERENCES wallets(id),
     amount BIGINT NOT NULL, -- Negative (withdraw/transfer) or positive (recharge)
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX idx_wallets_user_id ON wallets(user_id);
-CREATE INDEX idx_transfers_from_wallet_id ON transfers(from_wallet_id);
-CREATE INDEX idx_transfers_to_wallet_id ON transfers(to_wallet_id);
-CREATE INDEX idx_entries_wallet_id ON entries(wallet_id);
+CREATE INDEX IF NOT EXISTS idx_wallets_user_id ON wallets(user_id);
+CREATE INDEX IF NOT EXISTS idx_transfers_from_wallet_id ON transfers(from_wallet_id);
+CREATE INDEX IF NOT EXISTS idx_transfers_to_wallet_id ON transfers(to_wallet_id);
+CREATE INDEX IF NOT EXISTS idx_entries_wallet_id ON entries(wallet_id);
