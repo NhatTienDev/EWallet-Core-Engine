c001496fccc5a1ac3eb4cca19fd69643ccbc7c3e
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-06-01 18:39:24 +0700

    Implement CreateWallet func of WalletUseCase interface

---
+++ backend/internal/wallet/usecase/create_wallet.go
@@ -0,0 +1,27 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/nhattiendev/ewallet/internal/wallet/domain"
+)
+
+func (u *walletUseCase) CreateWallet(ctx context.Context, userID int64, currency string) (*domain.Wallet, error) {
+	if currency == "" {
+		currency = "VND"
+	}
+
+	// Create a new wallet with zero balance
+	newWallet := &domain.Wallet{
+		UserID: userID,
+		Balance: 0,
+		Currency: currency,
+	}
+
+	err := u.walletRepo.CreateWallet(ctx, newWallet)
+	if err != nil {
+		return nil, err
+	}
+
+	return newWallet, nil
+}
