f993e7c9ed1c314cd4046df09b1e5ab2cc684e55
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-06-02 15:23:04 +0700

    Implement GetTransferHistory func of WalletUseCase interface

---
+++ backend/internal/wallet/usecase/get_transfer_history.go
@@ -0,0 +1,16 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/nhattiendev/ewallet/internal/wallet/domain"
+)
+
+func (u *walletUseCase) GetTransferHistory(ctx context.Context, walletID int64, limit, offset int32) ([]domain.Tranfer, error) {
+	_, err := u.walletRepo.GetWalletByID(ctx, walletID)
+	if err != nil {
+		return nil, err
+	}
+
+	return u.walletRepo.GetListTransfers(ctx, walletID, limit, offset)
+}
