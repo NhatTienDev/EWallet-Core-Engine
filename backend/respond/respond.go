0080a2859d15f2a980e4f7b701f01ecb3c83b90d
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-06-04 20:13:59 +0700

    Create shared respond.go file to handle respond json data

---
+++ backend/respond/respond.go
@@ -0,0 +1,36 @@
+package respond
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+type apiResponse struct {
+	Error string `json:"error,omitempty"`
+	Message string `json:"message,omitempty"`
+	Data any `json:"data,omitempty"`
+}
+
+// Write a cleaner JSON response for the client
+func WriteSuccessJSON(w http.ResponseWriter, status int, message string, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	res := apiResponse{
+		Message: message,
+		Data: data,
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func WriteErrorJSON(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	res := apiResponse{
+		Message: message,
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
