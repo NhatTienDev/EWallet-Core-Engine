76f602c9587c1ce78b214d8bcebc69f7b232a2ba
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-06-06 20:45:06 +0700

    Add channel (new field) to receive user_id

--- backend/internal/user/usecase/user_usecase.go
+++ backend/internal/user/usecase/user_usecase.go
@@ -5,11 +5,13 @@ import "github.com/nhattiendev/ewallet/internal/user/domain"
 type userUseCase struct {
 	userRepo domain.UserRepository
 	jwtSecretKey []byte // Receive from main
+	userCreatedChan chan<- int64 // Channel receives user_id
 }
 
-func NewUserUseCase(userRepo domain.UserRepository, jwtSecretKey string) domain.UserUseCase {
+func NewUserUseCase(userRepo domain.UserRepository, jwtSecretKey string, userCreatedChan chan<- int64) domain.UserUseCase {
 	return &userUseCase{
 		userRepo: userRepo,
 		jwtSecretKey: []byte(jwtSecretKey),
+		userCreatedChan: userCreatedChan,
 	}
 }
