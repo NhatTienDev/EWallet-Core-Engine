cd428526997342e7e649afbb10439dd2e81b5438
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-06-16 21:19:31 +0700

    Add queries support for password reset

--- backend/database/query/users.sql
+++ backend/database/query/users.sql
@@ -11,8 +11,23 @@ WHERE email = $1;
 SELECT * FROM users
 WHERE id = $1;
 
--- name: UpdateUserPassword :one
+-- name: CreatePasswordReset :one
+INSERT INTO password_resets (user_id, hashed_token, expires_at)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetValidPasswordReset :one
+-- Get token that is not used and not expired
+SELECT * FROM password_resets
+WHERE hashed_token = $1 AND is_used = FALSE AND expires_at > NOW()
+LIMIT 1;
+
+-- name: MarkPasswordResetUsed :exec
+UPDATE password_resets
+SET is_used = TRUE
+WHERE id = $1;
+
+-- name: UpdateUserPassword :exec
 UPDATE users
 SET hashed_password = $2
-WHERE id = $1
-RETURNING *;
+WHERE id = $1;
