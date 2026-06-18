618a9658fe40cc9a4e64cd95cd98274af61caea5
tiennguyen20020713 <tien.nguyen20020713@gmail.com>
2026-06-18 19:39:20 +0700

    Add mailpitSenderRepo field

--- backend/internal/user/usecase/user_usecase.go
+++ backend/internal/user/usecase/user_usecase.go
@@ -4,13 +4,15 @@ import "github.com/nhattiendev/ewallet/internal/user/domain"
 
 type userUseCase struct {
 	userRepo domain.UserRepository
+	mailpitSenderRepo domain.MailpitSenderRepository
 	jwtSecretKey []byte // Receive from main
 	userCreatedChan chan<- int64 // Channel receives user_id
 }
 
-func NewUserUseCase(userRepo domain.UserRepository, jwtSecretKey string, userCreatedChan chan<- int64) domain.UserUseCase {
+func NewUserUseCase(userRepo domain.UserRepository, mailpitSenderRepo domain.MailpitSenderRepository, jwtSecretKey string, userCreatedChan chan<- int64) domain.UserUseCase {
 	return &userUseCase{
 		userRepo: userRepo,
+		mailpitSenderRepo: mailpitSenderRepo,
 		jwtSecretKey: []byte(jwtSecretKey),
 		userCreatedChan: userCreatedChan,
 	}
